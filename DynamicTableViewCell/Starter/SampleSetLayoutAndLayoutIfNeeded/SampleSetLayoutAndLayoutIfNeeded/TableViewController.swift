2d881da1b5b5577f8d0f037aee0ee8f214dfdd5e
Hula Nguyen <>
2021-06-10 16:20:17 +0700

    Updated tips for dynamic cell height for table view base on the content of label

---
+++ DynamicTableViewCell/Starter/SampleSetLayoutAndLayoutIfNeeded/SampleSetLayoutAndLayoutIfNeeded/TableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewController.swift
+//  SampleSetLayoutAndLayoutIfNeeded
+//
+//  Created by Lam Nguyen Huu (VN) on 10/06/2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+let sampleData = [
+    "Discussion \nThe default implementation of this method does nothing on iOS 5.1 and earlier. Otherwise, the default implementation uses any constraints you have set to determine the size and position of any subviews.",
+    "Subclasses can override this method as needed to perform more precise layout of their subviews. You should override this method only if the autoresizing and constraint-based behaviors of the subviews do not offer the behavior you want. You can use your implementation to set the frame rectangles of your subviews directly.",
+    "You should not call this method directly. If you want to force a layout update, call the setNeedsLayout() method instead to do so prior to the next drawing update. If you want to update the layout of your views immediately, call the layoutIfNeeded() method.",
+    "Discussion\nThe default implementation of this method does nothing on iOS 5.1 and earlier. Otherwise, the default implementation uses any constraints you have set to determine the size and position of any subviews.\n\nSubclasses can override this method as needed to perform more precise layout of their subviews. You should override this method only if the autoresizing and constraint-based behaviors of the subviews do not offer the behavior you want. You can use your implementation to set the frame rectangles of your subviews directly.\n\nYou should not call this method directly. If you want to force a layout update, call the setNeedsLayout() method instead to do so prior to the next drawing update. If you want to update the layout of your views immediately, call the layoutIfNeeded() method."
+]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        cell.textLabel?.text = sampleData[indexPath.row]
+        return cell
+    }
+}
---
+++ DynamicTableViewCell/Starter/SampleSetLayoutAndLayoutIfNeeded/SampleSetLayoutAndLayoutIfNeeded/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSetLayoutAndLayoutIfNeeded
+//
+//  Created by Lam Nguyen Huu (VN) on 10/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
